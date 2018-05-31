f2b698a5737b5eca59267879fd1499581d812762
Geripii <33180486+Geripii@users.noreply.github.com>
2018-05-31 13:31:02 +0100

    Add more test cases

--- src/test/java/LoginPageTest/LoginPageTestDriver.java
+++ src/test/java/LoginPageTest/LoginPageTestDriver.java
@@ -1,6 +1,24 @@
 package LoginPageTest;
 
 import BaseTestData.BaseData;
+import org.openqa.selenium.By;
+import org.testng.Assert;
 
 public class LoginPageTestDriver extends BaseData {
+
+
+    public void login(String TESTUSERNAME, String TESTPASSWORD){
+        driver.findElement(By.id("username")).sendKeys(TESTUSERNAME);
+        driver.findElement(By.id("password")).sendKeys(TESTPASSWORD);
+        driver.findElement(By.className("btn-primary")).click();
+    }
+
+    public void findInvalidMessage(){
+        driver.findElement(By.className("alert-danger"));
+    }
+
+    public void urlIsAssert(String checkPage){
+
+        Assert.assertEquals(driver.getCurrentUrl(), checkPage);
+    }
 }
