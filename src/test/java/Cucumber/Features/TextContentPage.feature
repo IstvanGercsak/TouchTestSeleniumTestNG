fbdfdb3ed40da1f9610c49f1278ac6a31a2c8424
Geripii <33180486+Geripii@users.noreply.github.com>
2018-08-31 10:29:26 +0100

    Expand test

--- src/test/java/Cucumber/Features/TextContentPage.feature
+++ src/test/java/Cucumber/Features/TextContentPage.feature
@@ -16,3 +16,29 @@ Feature: Text Content Page features
     Given I log into the application with Jurisdiction user
     And I go on the "Text Content" page
     Then The "Text Content" breadcrumb appears
+
+  Scenario Outline: Text content content
+    Given I navigate to the login page
+    And I log into the Touch with "TestAdmin@TestAdmin.com" username and "Igercsak8!#" password
+    And I click on the login button
+    And I go on the "Text Content" page
+    And I click on the <text_id> text id
+    And I can see the right <Text_locale> text locale, <text_title> title, <text_title> text title and <text_description> text description
+    And I click on the modification pencil button
+    And I click on the Cancel button
+    And I click on the modification pencil button
+    And I modify the Resource Title to <title_modification_to> and the Resource Description to <text_description_modification_to>
+    And I click on the Save button
+    And I can see the right <Text_locale> text locale, <title_modification_to> title, <title_modification_to> text title and <text_description_modification_to> text description
+    And I click on the modification pencil button
+    And I modify the Resource Title to <text_title> and the Resource Description to <text_description>
+    And I click on the Save button
+    And I can see the right <Text_locale> text locale, <text_title> title, <text_title> text title and <text_description> text description
+
+    Examples:
+      | text_id                                         | Text_locale | text_title            | text_description      | title_modification_to        | text_description_modification_to      |
+      | base:tlbl:central:about-build-number            | en_US       | Touch build number    | Touch build number    | Touch build number_test12    | Touch build number_descriptiontest    |
+      | base:tlbl:central:about-build-timestamp         | en_US       | Touch build timestamp | Touch build timestamp | Touch build timestamp_test12 | Touch build timestamp_descriptiontest |
+      | base:tlbl:central:about-build-version           | en_US       | Touch build version   | Touch build version   | Touch build version_test12   | Touch build version_descriptiontest   |
+      | base:tlbl:central:admin-content-templates-title | en_US       | Content Templates     | Content Templates     | Content Templates_test12     | Content Templates_descriptiontest     |
+      | base:tlbl:central:admin-content-text-title      | en_US       | Text Content          | Text Content          | Text Content_test12          | Text Content_descriptiontest          |
--- src/test/java/Cucumber/Steps/TextContentPage.java
+++ src/test/java/Cucumber/Steps/TextContentPage.java
@@ -1,7 +1,13 @@
 package Cucumber.Steps;
 
 import Cucumber.Base.BaseUtil;
+import cucumber.api.PendingException;
+import cucumber.api.java.en.And;
 import cucumber.api.java.en.Then;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 
 public class TextContentPage extends BaseUtil {
@@ -16,6 +22,72 @@ public class TextContentPage extends BaseUtil {
     public void iArriveOnTheTextContentListsPage() {
 
         Assert.assertEquals(base.driver.getCurrentUrl(), TEXTCONTENTPAGE());
+
+    }
+
+    @And("^I click on the ([^\"]*) text id$")
+    public void iClickOnTheText_idTextId(String textID) {
+
+        base.driver.findElement(By.linkText(textID)).click();
+
+    }
+
+    @And("^I can see the right ([^\"]*) text locale, ([^\"]*) title, ([^\"]*) text title and ([^\"]*) text description$")
+    public void iCanSeeTheRightText_localeTextLocaleText_titleTitleText_titleTextTitleAndText_descriptionTextDescription(
+            String textLocale,
+            String mainTextTitle,
+            String textTitle,
+            String textDescription) throws InterruptedException {
+
+        Thread.sleep(1000);
+
+        Assert.assertEquals(base.driver.findElement(By.xpath("//*[@id=\"defaultFragment\"]/div[3]/div/div/section/div[2]/div/table/tbody/tr/td[1]/span")).getText(), textLocale);
+        Assert.assertEquals(base.driver.findElement(By.className("header3-font")).getText(), mainTextTitle);
+        Assert.assertEquals(base.driver.findElement(By.xpath("//*[@id=\"defaultFragment\"]/div[3]/div/div/section/div[2]/div/table/tbody/tr/td[2]/span")).getText(), textTitle);
+        Assert.assertEquals(base.driver.findElement(By.xpath("//*[@id=\"defaultFragment\"]/div[3]/div/div/section/div[2]/div/table/tbody/tr/td[3]/span")).getText(), textDescription);
+
+    }
+
+    @And("^I click on the modification pencil button$")
+    public void iClickOnTheModificationPencilButton() throws InterruptedException {
+
+        Thread.sleep(1000);
+
+        base.driver.findElement(By.id("updateModalButton")).click();
+
+    }
+
+    @And("^I click on the Cancel button$")
+    public void iClickOnTheCancelButton() {
+
+        WebDriverWait wait = new WebDriverWait(base.driver, 10);
+        WebElement element = wait.until(ExpectedConditions.elementToBeClickable(By.className("btn-primary")));
+
+        base.driver.findElement(By.className("btn-primary")).click();
+
+    }
+
+    @And("^I click on the Save button$")
+    public void iClickOnTheSaveButton() {
+
+        base.driver.findElement(By.id("saveUpdate")).click();
+
+    }
+
+    @And("^I modify the Resource Title to ([^\"]*) and the Resource Description to ([^\"]*)$")
+    public void iModifyTheResourceTitleToTitle_modification_toAndTheResourceDescriptionToText_description_modification_to(
+            String resourceTitle,
+            String resourceDescription) throws InterruptedException {
+
+        Thread.sleep(1000);
+
+        base.driver.findElement(By.id("valueUpdateResourceTitle")).click();
+        base.driver.findElement(By.id("valueUpdateResourceTitle")).clear();
+        base.driver.findElement(By.id("valueUpdateResourceTitle")).sendKeys(resourceTitle);
+
+        base.driver.findElement(By.id("valueUpdateResourceDescription")).click();
+        base.driver.findElement(By.id("valueUpdateResourceDescription")).clear();
+        base.driver.findElement(By.id("valueUpdateResourceDescription")).sendKeys(resourceDescription);
 
     }
 }
